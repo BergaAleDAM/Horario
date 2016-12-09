e958e1b19acb26fc6919b2907fa437c59cf9e468
ALUMNEDAM <titoberguis@gmail.com>
2016-12-09 19:21:17 +0100

    SOLO acabarlo

--- app/src/main/java/com/example/alumnedam/horario/Basededatos.java
+++ app/src/main/java/com/example/alumnedam/horario/Basededatos.java
@@ -3,24 +3,37 @@ package com.example.alumnedam.horario;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-
 /**
  * Created by ALUMNEDAM on 01/12/2016.
  */
 
-public class Basededatos extends SQLiteOpenHelper{
+public class  Basededatos extends SQLiteOpenHelper{
 
     String[] sqlCreate = new String[3];
-
+    String[] sqlInserts = new String[30];
     public Basededatos (Context context, String name, SQLiteDatabase.CursorFactory factory, int version){
         super(context,name,factory,version);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase){
-        slqCreate[0] = "CREATE TABLE PROFESORES (id_profe INTEGER, nom_prof TEXT, cognoms_profe)";
-        slqCreate[1] = "CREATE TABLE ASIGNATURA (id_asignatura INTEGER, id_profe INTEGER, nom_prof TEXT, cognoms_profe)";
-        slqCreate[2] = "CREATE TABLE PROFESORES (id_profe INTEGER, nom_prof TEXT, cognoms_profe)";
+        sqlCreate[0] = "CREATE TABLE PROFESORES (id_profe INTEGER, nom_asig TEXT)";
+        sqlCreate[1] = "CREATE TABLE ASIGNATURA (id_asignatura INTEGER, id_profe INTEGER, nom_prof TEXT, cognoms_profe TEXT)";
+        sqlCreate[2] = "CREATE TABLE HORARI (id_horari INTEGER , id_subgrup TEXT, id_asignatura INTEGER, hora_inici TEXT, hora_fi TEXT, dia_setmana TEXT )";
+
+
+
+        sqlInserts[0]="INSERT INTO PROFESORES VALUES (1, 'Lluis Maria', 'Perpinya'), (2, 'Jorge', 'Rubio'), (3, 'Josefa', 'Gonzalez') , (4, 'Marta', 'Planas'), (5, 'Jose Antonio', 'Leo')";
+        sqlInserts[0]="INSERT INTO ASIGNATURA VALUES ('M3', 3, 'Programacion'),('M5-M2-M6' , 2,'Cosas que tienen que ver con programacion'),('M7', 5, 'PHP')," +
+                "('M8', 1, 'Programacio amb android'),('M9', 2, 'Hilos y procesos'),('M10', 4, 'Cosas con erp'), ('MPatio',NULL, 'Patio')";
+        sqlInserts[0]="INSERT INTO HORARI VALUES (1, 'A1', 'M5', '16:00:00', '16:59:59',3)," +
+                "(2, 'A1', 'M9', '17:00:00', '17:59:59', 3)" +
+                "(3, 'A1', 'M9', '18:20:00', '19:19:59', 3)" +
+                "(4, 'A1', 'M3', '19:20:00', '21:19:59', 3)" +
+                "(5, 'A2', 'M8', '17:00:00', '17:59:59', 3)" +
+                "(6, 'A2', 'M8', '18:20:00', '19:19:59', 3)" +
+                "(7, 'A2', 'M9', '19:20:00', '21:19:59', 3)" ;
+
 
     }
 
--- app/src/main/java/com/example/alumnedam/horario/MainActivity.java
+++ app/src/main/java/com/example/alumnedam/horario/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.alumnedam.horario;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Basededatos bd =   new Basededatos(this, "Il horario", null, 2);
+        SQLiteDatabase slq= bd.getWritableDatabase();
     }
 }
